63765d9647eb31bdcc7555d4acb2cc1348da636d
Bob131 <bob@bob131.so>
2016-07-22 16:43:01 +1000

    Add spi-dump.ino
    This commit adds the source file for the Arduino.

---
+++ spi-dump.ino
@@ -0,0 +1,35 @@
+#include <SPI.h>
+
+#define SPI_READ_CMD 0x03
+#define CS_PIN 10
+
+#define CMD_PREFIX 0xff
+#define CS_PULL_LOW  0x1
+#define CS_PULL_HIGH 0x2
+
+void setup() {
+    Serial.begin(57600);
+
+    pinMode(CS_PIN, OUTPUT);
+    digitalWrite(CS_PIN, HIGH);
+    SPI.begin();
+}
+
+void loop() {
+    if (Serial.available()) {
+        byte input = Serial.read();
+        if (input == CMD_PREFIX) {
+            while (Serial.available() == 0) {}
+            byte cmd = Serial.read();
+            if (cmd == CS_PULL_LOW)
+                digitalWrite(CS_PIN, LOW);
+            else if (cmd == CS_PULL_HIGH)
+                digitalWrite(CS_PIN, HIGH);
+            if (cmd != CMD_PREFIX) {
+                Serial.write(cmd xor 0xff);
+                return;
+            }
+        }
+        Serial.write(SPI.transfer(input));
+    }
+}
